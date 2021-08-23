0ee8dd5b496194e2c44e584e632cbabf4c0aad1a
Marcel Bargull <marcel.bargull@udo.edu>
2021-08-23 08:11:32 +0200

    package-downloads: Fix sort; add recent_downloads
    Also commit only once per day
    (but try more often to make up for server errors).

--- src/package_downloads/stats_from_anaconda_org.py
+++ src/package_downloads/stats_from_anaconda_org.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 
 from asyncio import gather, run
-from collections import defaultdict
-from datetime import datetime
+from collections import defaultdict, OrderedDict
+from dataclasses import dataclass
+from datetime import datetime, timedelta
 from functools import partial
 from itertools import chain, islice
 from json import dumps, loads
@@ -10,9 +11,9 @@ from logging import INFO, basicConfig, getLogger
 from pathlib import Path
 from sys import maxsize
 from types import TracebackType
-from typing import Any, Dict, List, Optional, Tuple, Type
+from typing import Any, Callable, Dict, ItemsView, List, Optional, Tuple, Type, Union
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientTimeout
 
 from .common import BASE_DIR, CHANNELS, DATE_FORMAT
 from .ntp_time import get_ntp_time
@@ -32,7 +33,7 @@ log = getLogger(__name__).info
 
 class Session:
     def __init__(self) -> None:
-        self.client_session = ClientSession()
+        self.client_session = ClientSession(timeout=ClientTimeout(total=15 * 60))
         self.date = get_ntp_time()
 
     async def __aenter__(self) -> "Session":
@@ -80,113 +81,267 @@ async def get_package_download_counts(
         version: str = package_file_info["version"]
         subdir: str = package_file_info["attrs"]["subdir"]
         basename: str = package_file_info["basename"].rsplit("/", 1)[-1]
-        downloads[(channel, package_name, version, subdir, basename, "dummy")] = max(
-            0, ndownloads
-        )
+        downloads[(channel, package_name, version, subdir, basename)] = max(0, ndownloads)
     return downloads
 
 
-def reduce_counts_level(
-    counts: Dict[Tuple[str, ...], int]
-) -> Dict[Tuple[str, ...], Dict[str, int]]:
-    reduced_counts: Dict[Tuple[str, ...], Dict[str, int]] = defaultdict(lambda: {})
-    for key, count in counts.items():
-        reduced_counts[key[:-1]][key[-1]] = count
+def get_path(path_tuple: Tuple[str, ...]) -> Path:
+    return Path(BASE_PATH).joinpath(*path_tuple[:-1], f"{path_tuple[-1]}.json")
+
+
+def from_list_of_dicts(
+    input_list: List[Dict[str, Any]], key_str: str, value_str: str
+) -> Dict[str, Any]:
+    return {entry[key_str]: entry[value_str] for entry in input_list}
+
+
+async def load_old_counts(path_tuple: Tuple[str, ...]) -> Dict[str, int]:
+    path = get_path(path_tuple)
+    if path.is_file():
+        return from_list_of_dicts(
+            loads(path.read_text()).get("downloads_per_date", []), "date", "total"
+        )
+    return {}
+
+
+def parse_date(date: str) -> datetime:
+    return datetime.strptime(date, DATE_FORMAT)
+
+
+def sort_by_date(counts_per_date: Dict[str, Any]) -> List[Tuple[str, Any]]:
+    return sorted(
+        counts_per_date.items(),
+        key=lambda date__total: (parse_date(date__total[0]), date__total[1]),
+    )
+
+
+def dedup_counts_per_date(counts_per_date: Dict[str, Any]) -> Dict[str, Any]:
+    # Ensure sorted by date.
+    counts_per_date_list = sort_by_date(counts_per_date)
+    # Remove redundant (no count change / same date) information.
+    deduped_counts_per_date = []
+    for prev_, curr_, next_ in zip(
+        chain([("0000-01-01", -1)], counts_per_date_list),
+        counts_per_date_list,
+        chain(islice(counts_per_date_list, 1, maxsize), [("9999-12-31", maxsize)]),
+    ):
+        if prev_[1] == curr_[1] == next_[1]:
+            continue
+        if curr_[0] == next_[0]:
+            continue
+        deduped_counts_per_date.append(curr_)
+    return OrderedDict(deduped_counts_per_date)
+
+
+def as_list(
+    input_dict: Dict[Any, Any],
+    key_str: str,
+    value_str: str,
+) -> List[Dict[str, Any]]:
+    output_list = []
+    for key, value in input_dict.items():
+        output_list.append({key_str: key, value_str: value})
+    return output_list
+
+
+async def save_json(path_tuple: Tuple[str, ...], data: Dict[str, Any]) -> None:
+    path = get_path(path_tuple)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    output = data.copy()
+    output.update(dict(zip(TOPOLOGY, path_tuple)))
+    json = dumps(output, indent=0, separators=(",", ":"), sort_keys=True)
+    path.write_text(json)
+
+
+async def update_basename_counts(
+    current_date: str,
+    current_basename_totals: Dict[Tuple[str, ...], int],
+) -> Dict[Tuple[str, ...], Dict[str, Any]]:
+    basename_counts_per_date = {}
+    for key, total in current_basename_totals.items():
+        counts_per_date = await load_old_counts(key)
+        counts_per_date.update({current_date: total})
+        counts_per_date = dedup_counts_per_date(counts_per_date)
+        await save_json(key, {"downloads_per_date": as_list(counts_per_date, "date", "total")})
+        basename_counts_per_date[key] = counts_per_date
+    return basename_counts_per_date
+
+
+def split_off_level_counts(
+    counts: Dict[Tuple[str, ...], Dict[str, Any]]
+) -> Dict[Tuple[str, ...], Dict[str, Dict[str, Any]]]:
+    reduced_counts: Dict[Tuple[str, ...], Dict[str, Any]] = defaultdict(dict)
+    for key, counts_for_level in counts.items():
+        reduced_counts[key[:-1]][key[-1]] = counts_for_level
     return reduced_counts
 
 
-async def update_counts(
-    date: str,
-    counts_per_level: Dict[Tuple[str, ...], int],
-    max_sub_category_entries: int = maxsize,
-) -> Dict[Tuple[str, ...], int]:
-    totals = {}
-    for key, counts in reduce_counts_level(counts_per_level).items():
-        total = sum(counts.values())
-        totals[key] = total
-        path = Path(BASE_PATH).joinpath(*key[:-1], f"{key[-1]}.json")
-        if path.is_file():
-            data = loads(path.read_text())
-        else:
-            path.parent.mkdir(parents=True, exist_ok=True)
-            data = dict(zip(TOPOLOGY, key))
-            data["downloads_per_date"] = []
-        downloads_per_date = data["downloads_per_date"]
-        downloads_per_date.append({"date": date, "total": total})
-        # Ensure sorted by date (should already be the case, but just to make sure).
-        downloads_per_date = sorted(
-            downloads_per_date,
-            key=lambda entry: (datetime.strptime(entry["date"], DATE_FORMAT), entry["total"]),
-        )
-        # Remove redundant (no count change / same date) information
-        cleaned_downloads_per_date = []
-        for prev_, curr_, next_ in zip(
-            chain([{"date": "0000-01-01", "total": -1}], downloads_per_date),
-            downloads_per_date,
-            chain(
-                islice(downloads_per_date, 1, maxsize),
-                [{"date": "9999-12-31", "total": maxsize}],
+def sort_by_date_and_counts(
+    counts_per_date: Dict[str, Dict[str, int]]
+) -> OrderedDict[str, OrderedDict[str, int]]:
+    return OrderedDict(
+        sorted(
+            (
+                (
+                    date,
+                    OrderedDict(
+                        sorted(
+                            ((key, total) for key, total in key_totals.items()),
+                            key=lambda key_total: key_total[1],
+                        )
+                    ),
+                )
+                for date, key_totals in counts_per_date.items()
             ),
-        ):
-            if prev_["total"] == curr_["total"] == next_["total"]:
-                continue
-            if curr_["date"] == next_["date"]:
-                continue
-            cleaned_downloads_per_date.append(curr_)
-        data["downloads_per_date"] = cleaned_downloads_per_date
-        if len(key) < len(TOPOLOGY):
-            sub_category = TOPOLOGY[len(key)]
-            downloads_per_sub_category = sorted(
-                [
-                    {sub_category: sub_category_key, "total": total}
-                    for sub_category_key, total in counts.items()
-                ],
-                key=lambda entry: next(iter(entry.items()))[::-1],
-            )
-            data[f"downloads_per_{sub_category}"] = downloads_per_sub_category[
-                :max_sub_category_entries
-            ]
-        json = dumps(data, indent=0, separators=(",", ":"), sort_keys=True)
-        path.write_text(json)
-    return totals
+            key=lambda key_value: parse_date(key_value[0]),
+        )
+    )
 
 
-async def update_package_counts(
-    channel: str, package_name: str, date: str, counts_per_basename: Dict[Tuple[str, ...], int]
-) -> Dict[Tuple[str, ...], int]:
-    log("update_package_counts: %s::%s", channel, package_name)
-    counts_per_level = counts_per_basename
-    for _ in range(len(TOPOLOGY) - 1):
-        counts_per_level = await update_counts(date, counts_per_level)
-    return counts_per_level
+def get_sub_counts_per_date(
+    all_sub_counts_per_date: Dict[str, Dict[str, int]],
+) -> OrderedDict[str, OrderedDict[str, int]]:
+    counts_per_date: Dict[str, Dict[str, int]] = defaultdict(dict)
+    for sub_key, sub_counts_per_date in all_sub_counts_per_date.items():
+        for date, total in sub_counts_per_date.items():
+            counts_per_date[date][sub_key] = total
+    return sort_by_date_and_counts(counts_per_date)
+
+
+def get_recent_counts(
+    session_date: str,
+    max_sub_level_entries: int,
+    max_days: int,
+    counts_per_date: OrderedDict[str, OrderedDict[str, int]],
+) -> OrderedDict[str, OrderedDict[str, int]]:
+    recent_counts_per_date: Dict[str, Dict[str, int]] = defaultdict(dict)
+    recent_total_counts: Dict[str, int] = {}
+    min_date = parse_date(session_date) - timedelta(days=max_days)
+    total_counts = counts_per_date[session_date]
+    prev_totals: Dict[str, int] = defaultdict(int)
+    for date, key_totals in counts_per_date.items():
+        if parse_date(date) < min_date:
+            continue
+        for key, total in key_totals.items():
+            if total > prev_totals[key]:
+                if key not in recent_total_counts:
+                    recent_total_counts[key] = total_counts[key] - total
+                recent_counts_per_date[date][key] = total
+                prev_totals[key] = total
+    recent_selection = sorted(
+        recent_total_counts.keys(), key=lambda key: recent_total_counts[key]
+    )[-max_sub_level_entries:]
+    selected_recent_counts_per_date = {}
+    for date, key_totals in recent_counts_per_date.items():
+        counts_at_date = {}
+        for key, total in key_totals.items():
+            if key in recent_selection:
+                counts_at_date[key] = total
+        if counts_at_date:
+            selected_recent_counts_per_date[date] = counts_at_date
+    return sort_by_date_and_counts(selected_recent_counts_per_date)
+
+
+def get_summed_up_counts_per_date(
+    session_date: str,
+    ordered_counts_per_date: OrderedDict[str, OrderedDict[str, int]],
+) -> Dict[str, int]:
+    tmp: Dict[str, int] = ordered_counts_per_date[session_date].copy()
+    summed_up_total = sum(tmp.values())
+    summed_up_counts_per_date = {}
+    for date, counts_at_date in reversed(ordered_counts_per_date.items()):
+        for key, total in counts_at_date.items():
+            summed_up_total -= tmp[key] - total
+            tmp[key] = total
+        summed_up_counts_per_date[date] = summed_up_total
+    return dedup_counts_per_date(summed_up_counts_per_date)
+
+
+async def update_counts(
+    session_date: str,
+    max_sub_level_entries: int,
+    max_days: int,
+    level_counts_per_date: Dict[Tuple[str, ...], Dict[str, Any]],
+) -> Dict[Tuple[str, ...], Dict[str, Any]]:
+    next_level_counts_per_date = {}
+    for key, all_sub_counts_per_date in split_off_level_counts(level_counts_per_date).items():
+        sub_level = TOPOLOGY[len(key)]
+        ordered_counts_per_date = get_sub_counts_per_date(all_sub_counts_per_date)
+        ordered_recent_counts_per_date = get_recent_counts(
+            session_date, max_sub_level_entries, max_days, ordered_counts_per_date
+        )
+        summed_up_counts_per_date = get_summed_up_counts_per_date(
+            session_date, ordered_counts_per_date
+        )
+        downloads_per_sub_level = dict(
+            sorted(
+                ordered_counts_per_date[session_date].items(),
+                key=lambda sub_key_total: sub_key_total[1],
+            )[-max_sub_level_entries:]
+        )
+        data = {
+            "downloads_per_date": as_list(summed_up_counts_per_date, "date", "total"),
+            f"downloads_per_{sub_level}": as_list(downloads_per_sub_level, sub_level, "total"),
+            "recent_downloads": as_list(
+                {
+                    date: as_list(recent_counts_at_date, sub_level, "total")
+                    for date, recent_counts_at_date in ordered_recent_counts_per_date.items()
+                },
+                "date",
+                f"downloads_per_{sub_level}",
+            ),
+        }
+        await save_json(key, data)
+        next_level_counts_per_date[key] = summed_up_counts_per_date
+    return next_level_counts_per_date
 
 
 async def save_package_stats(
-    session: Session, channel: str, package_name: str
-) -> Tuple[str, int]:
-    counts_per_basename = await get_package_download_counts(session, channel, package_name)
-    counts_per_version = await update_package_counts(
-        channel, package_name, session.date, counts_per_basename
+    session: Session,
+    max_sub_level_entries: int,
+    max_days: int,
+    channel: str,
+    package_name: str,
+) -> Dict[Tuple[str, ...], Dict[str, Any]]:
+    current_basename_totals = await get_package_download_counts(session, channel, package_name)
+    log("update_basename_counts: %s::%s", channel, package_name)
+    session_date = session.date
+    basename_counts_per_date = await update_basename_counts(
+        session_date, current_basename_totals
     )
-    return (package_name, sum(counts_per_version.values()))
+    level_counts_per_date = basename_counts_per_date
+    for _ in range(len(TOPOLOGY) - 2):
+        level_counts_per_date = await update_counts(
+            session_date, max_sub_level_entries, max_days, level_counts_per_date
+        )
+    return level_counts_per_date
 
 
 async def save_channel_stats(
     session: Session, channel_name: str, package_names: List[str]
 ) -> None:
-    counts_per_package: Dict[Tuple[str, ...], int] = {
-        (channel_name, package_name): total
-        for package_name, total in await gather(
-            *map(partial(save_package_stats, session, channel_name), package_names)
+    max_sub_level_entries = 50
+    max_days = 62
+    all_package_counts_per_date: Dict[Tuple[str, ...], Dict[str, Any]] = {}
+    for package_counts_per_date in await gather(
+        *map(
+            partial(save_package_stats, session, max_sub_level_entries, max_days, channel_name),
+            package_names,
         )
-    }
-    await update_counts(session.date, counts_per_package, max_sub_category_entries=200)
+    ):
+        all_package_counts_per_date.update(package_counts_per_date)
+    await update_counts(
+        session.date,
+        max_sub_level_entries,
+        max_days,
+        all_package_counts_per_date,
+    )
 
 
-async def main() -> None:
+async def main() -> str:
     channels = CHANNELS
     channel_package_names = {
-        channel_name: await retrieve_package_names(channel_url)
+        channel_name: (await retrieve_package_names(channel_url))
         for channel_name, channel_url in channels.items()
     }
     async with Session() as session:
